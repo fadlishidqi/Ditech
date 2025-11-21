5f0dc8127b1e39d17d61baed4d12372e12fd318f
fadlishidqi <fadlishidqifirdaus@gmail.com>
2025-11-21 16:03:11 +0700

    fix: correct namespace import for Section component in PortfolioForm

--- app/Filament/Resources/Portfolios/Schemas/PortfolioForm.php
+++ app/Filament/Resources/Portfolios/Schemas/PortfolioForm.php
@@ -5,12 +5,12 @@ namespace App\Filament\Resources\Portfolios\Schemas;
 use Filament\Forms\Components\DateTimePicker;
 use Filament\Forms\Components\FileUpload;
 use Filament\Forms\Components\MarkdownEditor;
-use Filament\Forms\Components\Section;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TagsInput;
 use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Toggle;
+use Filament\Schemas\Components\Section;
 use Filament\Schemas\Schema;
 use Illuminate\Support\Str;
 
